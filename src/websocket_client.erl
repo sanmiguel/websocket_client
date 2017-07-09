f75513e44133c8734c3c20154e07b6a43829a6bf
cookkkie <cookie.io@openmailbox.org>
2017-07-10 01:49:06 +0200

    Add start to websocket_client

--- src/websocket_client.erl
+++ src/websocket_client.erl
@@ -143,10 +143,55 @@ start_link(FsmName, URL, Handler, HandlerArgs, Opts) when is_list(Opts) ->
             Error
     end.
 
+%% @doc Start the websocket client
+%%
+%% URL : Supported schema: (ws | wss)
+%% Handler: module()
+%% Args : arguments to pass to Handler:init/1
+-spec start(URL :: string(), Handler :: module(), Args :: list()) ->
+    {ok, pid()} | {error, term()}.
+start(URL, Handler, Args) ->
+    start(URL, Handler, Args, []).
+
+%% @doc Start the websocket client
+%%
+%% Supported Opts:
+%%  - {keepalive, integer()}:  keepalive timeout in ms
+%%  - {extra_headers, list({K, V})}: a kv-list of headers to send in the handshake
+%% (useful if you need to add an e.g. 'Origin' header on connection.
+%%  - {ssl_verify, verify_none | verify_peer | {verify_fun, _}} : this is passed
+%%  through to ssl:connect/2,3.
+start(URL, Handler, HandlerArgs, Opts) ->
+    start(undefined, URL, Handler, HandlerArgs, Opts).
+
+%% @doc Start the websocket client
+%% see gen_fsm:start(FsmName, Module, Args, Options)
+%% FsmName = {local,Name} | {global,GlobalName} | {via,Module,ViaName}
+%%  Name = atom()
+%%  GlobalName = ViaName = term()
+%%  Module = atom()
+start(FsmName, URL, Handler, HandlerArgs, Opts) when is_binary(URL) ->
+  start(FsmName, binary_to_list(URL), Handler, HandlerArgs, Opts);
+start(FsmName, URL, Handler, HandlerArgs, Opts) when is_list(Opts) ->
+    case http_uri:parse(URL, [{scheme_defaults, [{ws,80},{wss,443}]}]) of
+        {ok, {Protocol, _, Host, Port, Path, Query}} ->
+            InitArgs = [Protocol, Host, Port, Path ++ Query, Handler, HandlerArgs, Opts],
+            %FsmOpts = [{dbg, [trace]}],
+            FsmOpts = [],
+            fsm_start(FsmName, InitArgs, FsmOpts);
+        {error, _} = Error ->
+            Error
+    end.
+
 fsm_start_link(undefined, Args, Options) ->
     gen_fsm:start_link(?MODULE, Args, Options);
 fsm_start_link(FsmName, Args, Options) ->
     gen_fsm:start_link(FsmName, ?MODULE, Args, Options).
+
+fsm_start(undefined, Args, Options) ->
+    gen_fsm:start_link(?MODULE, Args, Options);
+fsm_start(FsmName, Args, Options) ->
+    gen_fsm:start(FsmName, ?MODULE, Args, Options).
 
 send(Client, Frame) ->
     gen_fsm:sync_send_event(Client, {send, Frame}).
